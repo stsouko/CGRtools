28915eb7ca3ff141971871cd36f5d984039941c6
Ramil Nugmanov <stsouko@live.ru>
2018-09-09 19:46:04 +0300

    p-quinones added. pyroles fixing implemented. tests for aromatization added.

--- CGRtools/algorithms/aromatics.py
+++ CGRtools/algorithms/aromatics.py
@@ -18,6 +18,8 @@
 #  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 #  MA 02110-1301, USA.
 #
+from collections import defaultdict
+from itertools import repeat
 from .sssr import find_sssr
 
 
@@ -47,31 +49,45 @@ def _quinonize(g, rings):
         old = len(rings)
         found = []
         for n, r in enumerate(rings):
-            if len(r) != 6:
-                continue
+            if len(r) == 6:
+                r1, r2, r3, r4, r5, r6 = r
+                key = (g[r1][r2]['s_bond'], g[r2][r3]['s_bond'], g[r3][r4]['s_bond'], g[r4][r5]['s_bond'],
+                       g[r5][r6]['s_bond'], g[r6][r1]['s_bond'])
+                if 4 not in key:
+                    continue
 
-            r1, r2, r3, r4, r5, r6 = r
-            key = (g[r1][r2]['s_bond'], g[r2][r3]['s_bond'], g[r3][r4]['s_bond'], g[r4][r5]['s_bond'],
-                   g[r5][r6]['s_bond'], g[r6][r1]['s_bond'])
-            if 4 not in key:
-                continue
+                doubles = tuple(y for y, x in enumerate(r)
+                                if len(g[x]) == 3 and next(attr['s_bond'] for a, attr in g[x].items() if a not in r) == 2)
+                if not doubles:
+                    continue
 
-            doubles = tuple(y for y, x in enumerate(r)
-                            if len(g[x]) == 3 and next(attr['s_bond'] for a, attr in g[x].items() if a not in r) == 2)
-            if not doubles:
-                continue
-
-            ld = len(doubles)
-            if ld == 6:
-                g[r1][r2]['s_bond'] = g[r2][r3]['s_bond'] = g[r3][r4]['s_bond'] = 1
-                g[r4][r5]['s_bond'] = g[r5][r6]['s_bond'] = g[r6][r1]['s_bond'] = 1
-                found.append(n)
-            else:
-                if key in _quinone_pattern.get(doubles, {}):
-                    dear = _quinone_fix.get(doubles)
-                    g[r1][r2]['s_bond'], g[r2][r3]['s_bond'], g[r3][r4]['s_bond'], g[r4][r5]['s_bond'], \
-                    g[r5][r6]['s_bond'], g[r6][r1]['s_bond'] = dear
+                if len(doubles) == 6:
+                    g[r1][r2]['s_bond'] = g[r2][r3]['s_bond'] = g[r3][r4]['s_bond'] = 1
+                    g[r4][r5]['s_bond'] = g[r5][r6]['s_bond'] = g[r6][r1]['s_bond'] = 1
                     found.append(n)
+                else:
+                    if key in _quinone_pattern.get(doubles, {}):
+                        dear = _quinone_fix.get(doubles)
+                        g[r1][r2]['s_bond'], g[r2][r3]['s_bond'], g[r3][r4]['s_bond'], g[r4][r5]['s_bond'], \
+                        g[r5][r6]['s_bond'], g[r6][r1]['s_bond'] = dear
+                        found.append(n)
+            elif len(r) == 5:
+                r1, r2, r3, r4, r5 = r
+                key = (g[r1][r2]['s_bond'], g[r2][r3]['s_bond'], g[r3][r4]['s_bond'], g[r4][r5]['s_bond'],
+                       g[r5][r1]['s_bond'])
+                if 4 not in key:
+                    continue
+
+                positions = _pyrole_pattern.get(key)
+                if positions is None:
+                    continue
+
+                for m, pos in enumerate(positions):
+                    if g.nodes[r[pos]]['element'] in ('N', 'O', 'S', 'Se', 'P'):
+                        dear = _pyrole_fix[key][m]
+                        g[r1][r2]['s_bond'], g[r2][r3]['s_bond'], g[r3][r4]['s_bond'], g[r4][r5]['s_bond'], \
+                        g[r5][r1]['s_bond'] = dear
+                        found.append(n)
 
         for n in found[::-1]:
             del rings[n]
@@ -95,10 +111,10 @@ def _aromatize(g, rings):
                     found.append(n)
             elif len(r) == 5:
                 r1, r2, r3, r4, r5 = r
-                k = _pyrole.get((g[r1][r2]['s_bond'], g[r2][r3]['s_bond'], g[r3][r4]['s_bond'], g[r4][r5]['s_bond'],
-                                 g[r5][r1]['s_bond']))
+                position = _pyrole.get((g[r1][r2]['s_bond'], g[r2][r3]['s_bond'], g[r3][r4]['s_bond'],
+                                        g[r4][r5]['s_bond'], g[r5][r1]['s_bond']))
 
-                if k is not None and g.nodes[r[k]]['element'] in ('N', 'O', 'S', 'Se'):
+                if position is not None and g.nodes[r[position]]['element'] in ('N', 'O', 'S', 'Se', 'P'):
                     g[r1][r2]['s_bond'] = g[r2][r3]['s_bond'] = g[r3][r4]['s_bond'] = 4
                     g[r4][r5]['s_bond'] = g[r5][r1]['s_bond'] = 4
                     found.append(n)
@@ -149,13 +165,19 @@ _pyrole.update(zip(_clock((1, 4, 4, 2, 1)), _ind))
 _pyrole.update(zip(_clock((1, 2, 4, 4, 1)), _ind))
 _pyrole.update(zip(_clock((1, 4, 4, 4, 1)), _ind))
 
+# fixes after quinonize
+_pyrole.update(zip(_clock((4, 2, 4, 4, 4)), _ind))
+_pyrole.update(zip(_clock((4, 4, 4, 2, 4)), _ind))
+_pyrole.update(zip(_clock((4, 2, 4, 2, 4)), _ind))
+_pyrole.update(zip(_clock((4, 4, 2, 4, 4)), _ind))
+
 
 _quinone_pattern = {}
 _quinone_fix = {}
 
 
-_ind = ((0, 1), (0, 5), (4, 5), (3, 4), (2, 3), (1, 2))
-for i, *p in zip(_ind,
+_ind = ((0, 1), (0, 5), (4, 5), (3, 4), (2, 3), (1, 2))  # o-quinones
+for i, *p in zip(_ind, repeat((4, 4, 4, 4, 4, 4)),
                  _clock((4, 4, 4, 2, 4, 4)),
                  _clock((4, 4, 4, 4, 2, 4)),
                  _clock((4, 4, 2, 4, 4, 4)),
@@ -176,16 +198,45 @@ for i, *p in zip(_ind,
                  _clock((1, 1, 4, 4, 4, 4)),
                  _clock((1, 4, 4, 4, 4, 1))
                  ):
-    _quinone_pattern[i] = {(4, 4, 4, 4, 4, 4), *p}
+    _quinone_pattern[i] = set(p)
 
 _quinone_fix.update(zip(_ind, _clock((1, 1, 2, 1, 2, 1))))
 
 
-_ind = ((0, 1, 2, 3), (0, 1, 2, 5), (0, 1, 4, 5), (0, 3, 4, 5), (2, 3, 4, 5), (1, 2, 3, 4))
-for i, *p in zip(_ind,
+_ind = ((0, 1, 2, 3), (0, 1, 2, 5), (0, 1, 4, 5), (0, 3, 4, 5), (2, 3, 4, 5), (1, 2, 3, 4))  # 1,2,3,4-quinones
+for i, *p in zip(_ind, repeat((4, 4, 4, 4, 4, 4)),
                  _clock((1, 4, 1, 4, 4, 4)),
                  _clock((1, 4, 1, 4, 2, 4))
                  ):
-    _quinone_pattern[i] = {(4, 4, 4, 4, 4, 4), *p}
+    _quinone_pattern[i] = set(p)
 
 _quinone_fix.update(zip(_ind, _clock((1, 1, 1, 1, 2, 1))))
+
+_ind = ((0, 3), (2, 5), (1, 4))  # p-quinones
+for i, *p in zip(_ind, repeat((4, 4, 4, 4, 4, 4)),
+                 _clock((4, 2, 4, 4, 4, 4)),
+                 _clock((4, 2, 4, 4, 2, 4))
+                 ):
+    _quinone_pattern[i] = set(p)
+
+_quinone_fix.update(zip(_ind, _clock((1, 2, 1, 1, 2, 1))))
+
+
+# pyroles condensed with quinones fixes
+_ind = (0, 4, 3, 2, 1)
+_pyrole_pattern = defaultdict(list)
+_pyrole_fix = defaultdict(list)
+
+for i, *p in zip(_ind,
+                 zip(_clock((4, 1, 4, 4, 4)), _clock((1, 1, 1, 2, 1))),
+                 zip(_clock((4, 4, 4, 1, 4)), _clock((1, 2, 1, 1, 1))),
+                 zip(_clock((4, 1, 4, 2, 4)), _clock((1, 1, 1, 2, 1))),
+                 zip(_clock((4, 2, 4, 1, 4)), _clock((1, 2, 1, 1, 1))),
+                 zip(_clock((4, 1, 4, 1, 4)), repeat((1, 1, 1, 1, 1)))
+                 ):
+    for x, y in p:
+        _pyrole_pattern[x].append(i)
+        _pyrole_fix[x].append(y)
+
+_pyrole_pattern = dict(_pyrole_pattern)
+_pyrole_fix = dict(_pyrole_fix)
