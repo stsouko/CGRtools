8b69f43f92e8eebf3682e20b1153e2ca8d96e986
tagir <tagirshin@gmail.com>
2018-12-27 17:25:21 +0300

    added new rules

--- CGRtools/algorithms/standardize.py
+++ CGRtools/algorithms/standardize.py
@@ -64,8 +64,12 @@ query_patch = defaultdict(list)
 b1 = QueryBond()
 b2 = QueryBond()
 b3 = QueryBond()
+b14 = QueryBond()
+b124 = QueryBond()
 b2.order = 2
 b3.order = 3
+b14.order = (1, 4)
+b124.order = (1, 2, 4)
 
 # any atom need for full neighbors describing
 a = QueryAtom()
@@ -74,13 +78,14 @@ a.element = 'A'
 o2 = QueryAtom()
 o2.update(element='O', neighbors=1, hybridization=2)
 
-# Nitro [A]-N(=O)=O>>[A]-[N+](=O)-[O-]
+# N-Oxide [A]<-:>N(<-=:>[A])=O>>[A]-[N+](=O)-[O-]
 n = QueryAtom()
-n.update(element='N', neighbors=3, hybridization=3)
+n.update(element='N', neighbors=3, hybridization=(2, 3, 4))
 central[str(n)] = n
-query_patch[str(n)].extend(_prepare([(b2, o2), (b2, o2), (b1, a)], [{'charge': 1}, ({'order': 1}, {'charge': -1})]))
+query_patch[str(n)].extend(_prepare([(b2, o2), (b14, a), (b124, a)], [{'charge': 1}, ({'order': 1}, {'charge': -1})]))
 
-# Azide [A]-[N-]-[N+]#N>>[A]-N=[N+]=[N-]
+
+# Azide [N-]-[N+]#N>>N=[N+]=[N-]
 n1 = QueryAtom()
 n2 = QueryAtom()
 n3 = QueryAtom()
@@ -90,6 +95,104 @@ n3.update(element='N', neighbors=1, hybridization=3)
 central[str(n2)] = n2
 query_patch[str(n2)].extend(_prepare([(b1, n1), (b3, n3)],
                                      [{}, ({'order': 2}, {'charge': 0}), ({'order': 2}, {'charge': -1})]))
+
+# Diazo [C]-[N+]#N>>[C]-N=[N+]=[N-]
+c1 = QueryAtom()
+n2 = QueryAtom()
+n3 = QueryAtom()
+n3.update(element='N', neighbors=1, hybridization=3)
+n2.update(element='N', charge=1, neighbors=2, hybridization=3)
+c1.update(element='C', charge=-1, hybridization=1)
+central[str(n2)] = n2
+query_patch[str(n2)].extend(_prepare([(b3, n3), (b1, c1)],
+                                     [{}, ({'order': 2}, {'charge': -1}), ({'order': 2}, {'charge': 0})]))
+
+# Diazonium [C]-[N]=[N+]>>C-[N+]#[N]
+c1 = QueryAtom()
+n2 = QueryAtom()
+n3 = QueryAtom()
+c1.update(element='C')
+n2.update(element='N', neighbors=2, hybridization=2)
+n3.update(element='N', charge=1, neighbors=1, hybridization=2)
+central[str(n2)] = n2
+query_patch[str(n2)].extend(_prepare([(b2, n2), (b2, n3)],
+                                     [{}, ({'order': 3}, {'charge': 1}), ({'order': 3}, {'charge': 0})]))
+
+# Iminium [C+]-N>>C=[N+]
+c1 = QueryAtom()
+n2 = QueryAtom()
+c1.update(element='C', charge=1, neighbors=3, hybridization=1)
+n2.update(element='N', neighbors=3, hybridization=1)
+central[str(n2)] = n2
+query_patch[str(n2)].extend(_prepare([(b1, c1)], [{'charge': 1}, ({'order': 2}, {'charge': 0})]))
+
+# Isocyanate [N+]-[C-]=O>>N=C=O
+n1 = QueryAtom()
+c2 = QueryAtom()
+o3 = QueryAtom()
+n1.update(element='N', charge=1, neighbors=2, hybridization=1)
+c2.update(element='C', charge=-1, neighbors=2, hybridization=2)
+o3.update(element='O', hybridization=2)
+central[str(c2)] = c2
+query_patch[str(c2)].extend(_prepare([(b1, n1)], [{'charge': 0}, ({'order': 2}, {'charge': 0})]))
+
+# Nitrilium [C+]=N>>C#[N+]
+c1 = QueryAtom()
+n2 = QueryAtom()
+c1.update(element='C', charge=1, neighbors=2, hybridization=2)
+n2.update(element='N', neighbors=2, hybridization=2)
+central[str(n2)] = n2
+query_patch[str(n2)].extend(_prepare([(b1, c1)], [{'charge': 1}, ({'order': 3}, {'charge': 0})]))
+
+# Nitrone Nitronate [A]-N(=O)=O>>[A]-[N+](=A)-[O-]
+c1 = QueryAtom()
+n2 = QueryAtom()
+o3 = QueryAtom()
+c1.update(element='C', neighbors=3, hybridization=2)
+n2.update(element='N', neighbors=2, hybridization=1)
+o3.update(element='O', hybridization=2)
+central[str(n2)] = n2
+query_patch[str(n2)].extend(_prepare([(b2, o3)], [{'charge': 1}, ({'order': 1}, {'charge': -1})]))
+
+# Nitroso
+c1 = QueryAtom()
+n2 = QueryAtom()
+o3 = QueryAtom()
+c1.update(element='C', hybridization=(1, 2, 4))
+n2.update(element='N', charge=1, neighbors=2, hybridization=1)
+o3.update(element='O', charge=-1, hybridization=1)
+central[str(n2)] = n2
+query_patch[str(n2)].extend(_prepare([(b2, o3)], [{'charge': 0}, ({'order': 2}, {'charge': 0})]))
+
+#Phosphonic
+c1 = QueryAtom()
+p2 = QueryAtom()
+o3 = QueryAtom()
+o4 = QueryAtom()
+o5 = QueryAtom()
+c1.update(element='C',  hybridization=(1, 2, 4))
+p2.update(element='P', charge=1, neighbors=4, hybridization=1)
+o3.update(element='O', neighbors=2, hybridization=1)
+o4.update(element='O', neighbors=2, hybridization=1)
+o5.update(element='O', charge=-1, neighbors=1, hybridization=1)
+central[str(p2)] = p2
+query_patch[str(p2)].extend(_prepare([(b1, o5)], [{'charge': 0}, ({'order': 2}, {'charge': 0})]))
+
+#Phosphonic Ylide
+c1 = QueryAtom()
+p2 = QueryAtom()
+c3 = QueryAtom()
+c4 = QueryAtom()
+c5 = QueryAtom()
+c1.update(element='C')
+p2.update(element='P', charge=-1, neighbors=4, hybridization=1)
+c3.update(element='C')
+c4.update(element='C')
+c5.update(element='C', charge=1,  hybridization=1)
+central[str(p2)] = p2
+query_patch[str(p2)].extend(_prepare([(b1, c5)], [{'charge': 0}, ({'order': 2}, {'charge': 0})]))
+
+
 
 del b1, b2, b3, a, o2, n, n1, n2, n3
 
