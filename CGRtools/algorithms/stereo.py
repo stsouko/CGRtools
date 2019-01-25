63403105ea96679881870c99830b8ffbda834762
stsouko <stsouko@live.ru>
2019-01-25 12:15:06 +0300

    atropisomers detection.

--- CGRtools/algorithms/stereo.py
+++ CGRtools/algorithms/stereo.py
@@ -17,6 +17,7 @@
 #  along with this program; if not, see <https://www.gnu.org/licenses/>.
 #
 from collections import defaultdict
+from itertools import combinations, product
 from .morgan import primes
 from ..cache import cached_property
 
@@ -46,6 +47,7 @@ class Stereo:
         """
         print(self.__potentially_alkene)
         print(self.__potentially_tetrahedron)
+        print(self.__potentially_atropisomer)
         return
 
     @cached_property
@@ -57,6 +59,9 @@ class Stereo:
                 adj[n].append(m)
                 adj[m].append(n)
 
+        if not adj:
+            return [], []
+
         terminals = {x for x, y in adj.items() if len(y) == 1}
         cis_trans = []
         allene = []
@@ -66,8 +71,8 @@ class Stereo:
         #  second enter or None, second exit or None)
 
         # structure of cis_trans:
-        # ( first neighbor atom of enter atom, enter atom, exit atom, first neighbor atom of exit atom,
-        # first central atom, second central atom, second enter or None, second exit or None)
+        # (first neighbor atom of enter atom, enter atom, exit atom, first neighbor atom of exit atom,
+        #  first central atom, second central atom, second enter or None, second exit or None)
 
         while terminals:
             n = terminals.pop()
@@ -112,6 +117,55 @@ class Stereo:
                 continue
             chiral[n] = tuple(m_bond)
         return chiral
+
+    @cached_property
+    def __potentially_atropisomer(self):
+        if len(self.sssr) < 2:
+            return []
+        aromatic = [ring for ring in self.sssr if len(ring) in (5, 6) and self._adj[ring[0]][ring[-1]].order == 4
+                    and all(self._adj[n][m].order == 4 for n, m in zip(ring, ring[1:]))]
+        if len(aromatic) < 2:
+            return []
+
+        reduced_aromatic = [[x for x in x if len(self._adj[x]) == 3] for x in aromatic]  # remove :[CH]: atoms
+        connections = {}
+        for rings in combinations(range(len(reduced_aromatic)), 2):
+            ring1, ring2 = rings
+            for n, m in product(reduced_aromatic[ring1], reduced_aromatic[ring2]):
+                if n in self._adj[m]:
+                    if rings in connections:  # remove condensed rings or twice-bonded rings
+                        del connections[rings]
+                        break  # skip rings
+                    connections[rings] = (n, m)
+
+        # structure of atropisomer:
+        # (first neighbor atom of ring1 atom, ring1 atom, ring2 atom, first neighbor atom of ring2 atom,
+        #  second neighbor atom of ring1 atom, second neighbor atom of ring2 atom,
+        #  nonring neighbor of first neighbor atom of ring1 atom or None,
+        #  nonring neighbor of second neighbor atom of ring1 atom or None,
+        #  nonring neighbor of first neighbor atom of ring2 atom or None,
+        #  nonring neighbor of second neighbor atom of ring2 atom or None)
+
+        atropos = []
+        for (ring1, ring2), (n, m) in connections.items():
+            r1n, r1m = (x for x in self._adj[n] if x != m)
+            r2n, r2m = (x for x in self._adj[m] if x != n)
+
+            nr1n = next((x for x in self._adj[r1n] if x not in aromatic[ring1]), None)
+            nr1m = next((x for x in self._adj[r1m] if x not in aromatic[ring1]), None)
+            nr2n = next((x for x in self._adj[r2n] if x not in aromatic[ring2]), None)
+            nr2m = next((x for x in self._adj[r2m] if x not in aromatic[ring2]), None)
+
+            # skip rings without substituents
+            if nr1n is None and (nr1m is None or nr2n is None or nr2m is None):
+                continue
+            elif nr1m is None and (nr2n is None or nr2m is None):
+                continue
+            elif nr2n is nr2m:
+                continue
+            atropos.append((r1n, n, m, r2n, r1m, r2m, nr1n, nr1m, nr2n, nr2m))
+
+        return atropos
 
     def __tetrahedron(self):
         chiral = {}
