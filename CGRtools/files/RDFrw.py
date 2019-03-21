2c0bc1e0497222f6dabcb0b18891091bf5ed678c
dinar <batyrshin-dinar@mail.ru>
2019-03-21 13:38:59 +0300

    RDFread

--- CGRtools/files/RDFrw.py
+++ CGRtools/files/RDFrw.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 #
 #  Copyright 2014-2019 Ramil Nugmanov <stsouko@live.ru>
-#  Copyright 2018-2019 Dinar Batyrshin <batyrshin-dinar@mail.ru>
+#  Copyright 2019 Dinar Batyrshin <batyrshin-dinar@mail.ru>
 #  This file is part of CGRtools.
 #
 #  CGRtools is free software; you can redistribute it and/or modify
