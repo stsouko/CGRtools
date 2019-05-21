534a4ff7a41b8049864713b3d1f6ae1779f2e639
dinar <batyrshin-dinar@mail.ru>
2019-05-21 15:59:05 +0300

    render CGR

--- CGRtools/algorithms/depict.py
+++ CGRtools/algorithms/depict.py
@@ -62,10 +62,10 @@ class Depict:
                            f'y="{-max_y - 1.25 * self._render_font:.2f}" '
                            f'width="{width:.2f}" height="{height:.2f}" fill="white"/>')
                 svg.extend(masks)
-                svg.append('    </mask>\n  </defs>\n'
-                           f'  <g fill="none" stroke="black" stroke-width=".03"  mask="url(#mask-{uid})">')
+                svg.append(f'    </mask>\n  </defs>\n  <g fill="none" stroke="{self._render_bonds_color}" '
+                           f'stroke-width=".03"  mask="url(#mask-{uid})">')
             else:
-                svg.append('  <g fill="none" stroke="black" stroke-width=".03">')
+                svg.append(f'  <g fill="none" stroke="{self._render_bonds_color}" stroke-width=".03">')
             svg.extend(bonds)
             svg.append('  </g>')
 
@@ -101,26 +101,19 @@ class DepictMolecule(Depict):
             n, m = nodes[n], nodes[m]
             nx, ny, mx, my = n.x, n.y, m.x, m.y
             if bond.order in (1, 4):
-                svg.append(f'    <line x1="{nx:.2f}" y1="{-ny:.2f}" x2="{mx:.2f}" y2="{-my:.2f}" '
-                           f'stroke="{self._render_bonds_color}"/>')
+                svg.append(f'    <line x1="{nx:.2f}" y1="{-ny:.2f}" x2="{mx:.2f}" y2="{-my:.2f}"')
             elif bond.order == 2:
                 dx, dy = rotate_vector(0, self._render_double_space, mx - nx, my - ny)
-                svg.append(f'    <line x1="{nx + dx:.2f}" y1="{-ny + dy:.2f}" x2="{mx + dx:.2f}" y2="{-my + dy:.2f}"'
-                           f'stroke="{self._render_bonds_color}"/>')
-                svg.append(f'    <line x1="{nx - dx:.2f}" y1="{-ny - dy:.2f}" x2="{mx - dx:.2f}" y2="{-my - dy:.2f}"'
-                           f'stroke="{self._render_bonds_color}"/>')
+                svg.append(f'    <line x1="{nx + dx:.2f}" y1="{-ny + dy:.2f}" x2="{mx + dx:.2f}" y2="{-my + dy:.2f}"')
+                svg.append(f'    <line x1="{nx - dx:.2f}" y1="{-ny - dy:.2f}" x2="{mx - dx:.2f}" y2="{-my - dy:.2f}"')
             elif bond.order == 3:
                 dx, dy = rotate_vector(0, self._render_triple_space, mx - nx, my - ny)
-                svg.append(f'    <line x1="{nx + dx:.2f}" y1="{-ny + dy:.2f}" x2="{mx + dx:.2f}" y2="{-my + dy:.2f}"'
-                           f'stroke="{self._render_bonds_color}"/>')
-                svg.append(f'    <line x1="{nx:.2f}" y1="{-ny:.2f}" x2="{mx:.2f}" y2="{-my:.2f}"'
-                           f'stroke="{self._render_bonds_color}"/>')
-                svg.append(f'    <line x1="{nx - dx:.2f}" y1="{-ny - dy:.2f}" x2="{mx - dx:.2f}" y2="{-my - dy:.2f}"'
-                           f'stroke="{self._render_bonds_color}"/>')
+                svg.append(f'    <line x1="{nx + dx:.2f}" y1="{-ny + dy:.2f}" x2="{mx + dx:.2f}" y2="{-my + dy:.2f}"')
+                svg.append(f'    <line x1="{nx:.2f}" y1="{-ny:.2f}" x2="{mx:.2f}" y2="{-my:.2f}"')
+                svg.append(f'    <line x1="{nx - dx:.2f}" y1="{-ny - dy:.2f}" x2="{mx - dx:.2f}" y2="{-my - dy:.2f}"')
             else:
                 svg.append(f'    <line x1="{nx:.2f}" y1="{-ny:.2f}" x2="{mx:.2f}" y2="{-my:.2f}" '
-                           f'stroke-dasharray="{self._render_dashes[0]:.2f} {self._render_dashes[1]:.2f}"'
-                           f'stroke="{self._render_bonds_color}"/>')
+                           f'stroke-dasharray="{self._render_dashes[0]:.2f} {self._render_dashes[1]:.2f}"')
 
         for ring in self.aromatic_rings:
             c_x = sum(nodes[x].x for x in ring) / len(ring)
