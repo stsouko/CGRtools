86534afcfa159788b2366d2a8ca4862b7f69a1e4
stsouko <stsouko@live.ru>
2019-08-23 14:41:29 +0300

    aromatization reimplemented

--- CGRtools/algorithms/aromatics.py
+++ CGRtools/algorithms/aromatics.py
@@ -28,147 +28,58 @@ _pyrole_atoms = ('N', 'O', 'S', 'Se', 'P')
 class Aromatize:
     __slots__ = ()
 
-    def dummy_thiele(self):
-        """
-        convert structure to aromatic form (dummy algorithm. don't detect quinones)
-
-        :return: number of processed rings
-        """
-        bonds = self._bonds
-        atoms = self._atoms
-        unsaturated = {n for n, m_bond in bonds.items() if any(bond.order in (2, 4) for bond in m_bond.values())}
-        aromatics = []
-
-        for ring in self.sssr:
-            lr = len(ring)
-            if lr in (5, 6, 7):
-                if unsaturated.issuperset(ring):  # benzene, azulene, pyridine and quinones
-                    aromatics.append(ring)
-                else:  # pyrole, cyclopentapyridine and quinones
-                    sr = set(ring)
-                    if len(unsaturated & sr) == lr - 1:
-                        atom = atoms[(sr - unsaturated).pop()]
-                        if atom.atomic_symbol in _pyrole_atoms or atom.charge == -1 and atom.atomic_symbol == 'C':
-                            aromatics.append(ring)
-
-        if aromatics:
-            for ring in aromatics:
-                for n, m in zip(ring, ring[1:]):
-                    b = bonds[n][m]
-                    if b.order != 4:
-                        b._Bond__order = 4
-                b = bonds[ring[0]][ring[-1]]
-                if b.order != 4:
-                    b._Bond__order = 4
-            self.flush_cache()
-            return len(aromatics)
-        return 0
-
-    def thiele(self) -> int:
+    def thiele(self):
         """
         convert structure to aromatic form
-
-        :return: number of processed rings
         """
-        total = self.dummy_aromatize()
         bonds = self._bonds
-        atoms = self._atoms
-        patch = set()
-        double_bonded = {n for n, m_bond in bonds.items() if any(bond.order == 2 for bond in m_bond.values())}
+        sh = self._hybridizations
 
-        pyroles = set()
-        quinones = []
-        condensed_rings = defaultdict(lambda: defaultdict(list))
-        for ring in self.aromatic_rings:
-            ring = tuple(ring)
-            lr = len(ring)
-            if lr in (5, 6):
-                pyroles.update(n for n in ring if atoms[n].atomic_symbol in _pyrole_atoms or
-                               atoms[n].charge == -1 and atoms[n].atomic_symbol == 'C')
-            if not double_bonded.isdisjoint(ring):
-                quinones.append(ring)
+        rings = defaultdict(set)  # aromatic? skeleton. include quinones
+        for ring in self.sssr:
+            if len(ring) in (5, 6, 7) and all(sh[n] == 2 for n in ring):
+                n, *_, m = ring
+                rings[n].add(m)
+                rings[m].add(n)
+                for n, m in zip(ring, ring[1:]):
+                    rings[n].add(m)
+                    rings[m].add(n)
+        if not rings:
+            return
 
-            for n, m in zip(ring, ring[1:]):  # fill condensed rings graph
-                condensed_rings[n][m].append(ring)
-                condensed_rings[m][n].append(ring)
+        double_bonded = [n for n in rings if any(m not in rings and b.order == 2 for m, b in bonds[n].items())]
+        if double_bonded:  # delete quinones
+            for n in double_bonded:
+                for m in rings.pop(n):
+                    rings[m].discard(n)
+            while True:
+                try:
+                    n = next(n for n, ms in rings.items() if len(ms) == 1)
+                except StopIteration:
+                    break
+                m = rings.pop(n).pop()
+                pm = rings.pop(m)
+                pm.discard(n)
+                for x in pm:
+                    rings[x].discard(m)
+        if not rings:
+            return
+
+        rings = self._sssr(rings)  # search rings again
+        if not rings:
+            return
+
+        seen = set()
+        for ring in rings:
+            seen.update(ring)
             n, *_, m = ring
-            condensed_rings[n][m].append(ring)
-            condensed_rings[m][n].append(ring)
+            bonds[n][m]._Bond__order = 4
+            for n, m in zip(ring, ring[1:]):
+                bonds[n][m]._Bond__order = 4
+        for n in seen:
+            sh[n] = 4
 
-        while quinones:
-            ring = quinones.pop()
-            for n, m in zip(ring, ring[1:]):  # remove from condensed rings graph
-                condensed_rings[n][m].remove(ring)
-                condensed_rings[m][n].remove(ring)
-            n, *_, m = ring
-            condensed_rings[n][m].remove(ring)
-            condensed_rings[m][n].remove(ring)
-
-            doubles = [n for n, m in enumerate(ring) if m in double_bonded]
-            start = doubles[0]
-            if start:  # reorder double bonded to starting position
-                ordered_ring = ring[start:] + ring[:start]
-            else:
-                ordered_ring = ring
-
-            lr = len(ring)
-            if lr == 6:
-                unbalanced_ring = len(doubles) in (1, 3)
-            else:  # bis- or tetra- azulene 7-ring and pyrole or bis azulenes 5-rings quinones
-                unbalanced_ring = len(doubles) in (2, 4)
-
-            pyrole_like = not pyroles.isdisjoint(ring)
-
-            bond = 1
-            n = ordered_ring[0]
-            for m in ordered_ring[1:]:
-                if bond == 1:
-                    if m not in double_bonded:
-                        if unbalanced_ring:
-                            if pyrole_like:
-                                if m in pyroles:
-                                    unbalanced_ring = False
-                                else:
-                                    bond = 2
-                            elif condensed_rings[n][m]:
-                                unbalanced_ring = False
-                            else:
-                                bond = 2
-                        else:
-                            bond = 2
-                    if not condensed_rings[n][m]:
-                        patch.add((n, m, 1))
-                    elif n in double_bonded:  # found new quinone ring (Y)
-                        q = condensed_rings[n][m][0]
-                        if q not in quinones:
-                            quinones.append(q)
-                else:
-                    if m in double_bonded:
-                        raise InvalidAromaticRing(ring)
-                    if unbalanced_ring and not pyrole_like and condensed_rings[n][m]:
-                        unbalanced_ring = False
-                    else:
-                        bond = 1
-                    if not condensed_rings[n][m]:
-                        patch.add((n, m, 2))
-                        double_bonded.add(n)
-                        double_bonded.add(m)
-                        if condensed_rings[n][p]:
-                            q = condensed_rings[n][p][0]
-                            if q not in quinones:
-                                quinones.append(q)
-                p, n = n, m
-            else:
-                m = ordered_ring[0]
-                if bond != 1:
-                    raise InvalidAromaticRing(ring)
-                patch.add((n, m, 1))
-
-        if patch:
-            for n, m, b in patch:
-                bonds[n][m]._Bond__order = b
-            self.flush_cache()
-        return total
+        self.flush_cache()
 
     def kekule(self):
         """
