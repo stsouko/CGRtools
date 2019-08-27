44d0a1be0253d286a1f159e66eca34a6c3cba42c
stsouko <stsouko@live.ru>
2019-08-27 11:04:16 +0300

    saved

--- CGRtools/periodictable/groupXIV.py
+++ CGRtools/periodictable/groupXIV.py
@@ -68,8 +68,7 @@ class Si(Element, PeriodIII, GroupXIV):
 
     @property
     def _valences_exceptions(self):
-        return ((-2, False, 0, ((1, 'F'), (1, 'F'), (1, 'F'), (1, 'F'), (1, 'F'), (1, 'F'))),
-                (1, False, 0, ((1, 'O'), (1, 'O'), (1, 'O'))))
+        return (-2, False, 0, ((1, 'F'), (1, 'F'), (1, 'F'), (1, 'F'), (1, 'F'), (1, 'F'))),
 
 
 class Ge(Element, PeriodIV, GroupXIV):
