f72d5665217f68777cb8d3d2a1b13c9243269f60
Ramil Nugmanov <stsouko@live.ru>
2019-08-31 15:45:18 +0300

    CGRReactor fixed.

--- CGRtools/reactor.py
+++ CGRtools/reactor.py
@@ -22,160 +22,110 @@ from functools import reduce
 from itertools import chain, count, islice, permutations, product
 from logging import warning, info
 from operator import or_
+from typing import Union
 from .containers import QueryContainer, QueryCGRContainer, MoleculeContainer, CGRContainer, ReactionContainer
+from .periodictable import Element, DynamicElement
 
 
-class CGRreactor:
+class CGRReactor:
     """
     CGR based editor for CGRs and molecules.
     generates transformation from input CGR/molecule
     using template (CGRtools ReactionContainer)
     -----------------------------------------------------
     input: transformation template, CGR/molecule to edit
-    output: edited CGR/molecule or generator
+    output: generator of edited CGRs/molecules
     -----------------------------------------------------
     template should contain one reactant and one product:
-    CGRreactor allows only 1 -> 1 transformation
+    CGRReactor allows only 1 -> 1 transformation
 
-    CGRreactor init prepares the reactor container:
+    CGRReactor init prepares the reactor container:
 
-    >> reactor = CGRreactor(template, delete_atoms=True)
+    >> reactor = CGRReactor(template, delete_atoms=True)
 
-    CGRreactor calling transforms reactants to products and
-    returns generator of all possible products if limit=0,
-    one product if limit=1, else limited to number list of products:
-
-    >> products = reactor(structure, limit=0)  # generator
-    >> product = reactor(structure, limit=1)   # one product
-    >> products = reactor(structure, limit=5)  # list with 5 products
-
-
+    CGRReactor calling transforms reactants to products and
+    returns generator of all possible products.
     """
-    def __init__(self, template, delete_atoms=False):
+    def __init__(self, template: ReactionContainer, delete_atoms: bool = False):
         """
         :param template: CGRtools ReactionContainer
         :param delete_atoms: if True atoms exists in reactant but
                             not exists in product will be removed
         """
-        pattern, atom_attrs, bond_attrs, conditional_element, is_cgr, to_delete = self.__prepare_template(template)
+        pattern, elements, atom_attrs, bond_attrs, is_cgr, to_delete = self.__prepare_template(template)
         self.__pattern = pattern
+        self.__elements = elements
         self.__atom_attrs = atom_attrs
         self.__bond_attrs = bond_attrs
-        self.__conditional_element = conditional_element
         self.__is_cgr = is_cgr
         self.__to_delete = delete_atoms and to_delete or set()
         self.__meta = template.meta.copy()
 
-    def __call__(self, structure, limit=1, skip_intersection=True):
+    def __call__(self, structure: Union[MoleculeContainer, CGRContainer], automorphism_filter: bool = True):
         if not isinstance(structure, (MoleculeContainer, CGRContainer)):
             raise TypeError('only Molecules and CGRs possible')
 
-        mapping = self.__pattern.get_substructure_mapping(structure, limit)
-        if limit == 1:
-            if mapping:
-                return self.patcher(structure, mapping)
-        else:
-            if skip_intersection:
-                g = (self.patcher(structure, m) for m in skip(mapping))
-            else:
-                g = (self.patcher(structure, m) for m in mapping)
-
-            if limit > 1:
-                return list(islice(g, limit))
-            else:
-                return g
+        for mapping in self.__pattern.get_mapping(structure, automorphism_filter=automorphism_filter):
+            yield self._patcher(structure, mapping)
 
     @staticmethod
     def __prepare_template(template):
         if not template.reactants or not template.products:
             raise ValueError('empty template')
-        reactants = reduce(or_, template.reactants, QueryContainer())
-        products = reduce(or_, template.products, QueryContainer())
-
-        if isinstance(reactants, QueryCGRContainer):
-            if isinstance(products, QueryContainer):
-                products = QueryCGRContainer(products)
+        if any(isinstance(x, (CGRContainer, QueryCGRContainer)) for x in chain(template.reactants, template.products)):
+            reactants = reduce(or_, template.reactants, QueryCGRContainer())
+            products = reduce(or_, template.products, QueryCGRContainer())
             is_cgr = True
-        elif isinstance(products, QueryCGRContainer):
-            if isinstance(reactants, QueryContainer):
-                reactants = QueryCGRContainer(reactants)
-            is_cgr = True
+            e = DynamicElement
         else:
+            reactants = reduce(or_, template.reactants, QueryContainer())
+            products = reduce(or_, template.products, QueryContainer())
             is_cgr = False
+            e = Element
 
         to_delete = set(reactants).difference(products)
-
-        absolute_atom = defaultdict(dict)
-        conditional_element = {}
-
-        for n in set(products).difference(reactants):
-            # if unique atoms in patch has variable properties exception is raised
-            atom = products.atom(n)
-            if atom.element is None or len(atom.element) > 1:
-                raise ValueError('new atoms in patch should be static')
-            elif atom.neighbors or atom.hybridization:
+        atoms = defaultdict(dict)
+        elements = {}
+        for n, atom in products.atoms():
+            if atom.neighbors or atom.hybridization:
                 warning('neighbors and hybridization for new atoms unusable')
-            absolute_atom[n].update(element=atom.element[0], charge=atom.charge,
-                                    isotope=atom.isotope, multiplicity=atom.multiplicity,
-                                    x=atom.x, y=atom.y, z=atom.z, stereo=atom.stereo)
+            atoms[n].update(charge=atom.charge, is_radical=atom.is_radical)
+            elements[n] = e.from_atomic_number(atom.atomic_number)(atom.isotope)
+            if n not in reactants:
+                atoms[n]['xy'] = atom.xy
             if is_cgr:
-                absolute_atom[n].update(p_charge=atom.p_charge, p_multiplicity=atom.p_multiplicity,
-                                        p_x=atom.p_x, p_y=atom.p_y, p_z=atom.p_z, p_stereo=atom.p_stereo)
+                atoms[n].update(p_is_radical=atom.p_is_radical, p_charge=atom.p_charge)
 
-        for n in set(products).intersection(reactants):
-            r_atom = reactants.atom(n)
-            atom = products.atom(n)
+        bonds = list(products.bonds())
+        return reactants, elements, dict(atoms), bonds, is_cgr, to_delete
 
-            if atom.element:
-                if len(r_atom.element) == 1:
-                    absolute_atom[n]['element'] = atom.element[0]
-                elif len(r_atom.element) != len(atom.element):
-                    raise ValueError('element mapping invalid. possible A>A, E>A, A>E, [E]>A, [E]>[E]')
-                else:
-                    conditional_element[n] = dict(zip(r_atom.element, atom.element))
-            if atom.isotope:
-                absolute_atom[n]['isotope'] = atom.isotope
+    def _patcher(self, structure, mapping):
+        mapping = mapping.copy()
+        elements = self.__elements
 
-            absolute_atom[n]['charge'] = atom.charge
-            absolute_atom[n]['stereo'] = atom.stereo
-            if is_cgr:
-                absolute_atom[n]['p_charge'] = atom.p_charge
-                absolute_atom[n]['p_stereo'] = atom.p_stereo
-                if atom.multiplicity or atom.p_multiplicity:
-                    absolute_atom[n]['multiplicity'] = atom.multiplicity
-                    absolute_atom[n]['p_multiplicity'] = atom.p_multiplicity
-            elif atom.multiplicity:  # replace for fixed value
-                absolute_atom[n]['multiplicity'] = atom.multiplicity
+        plane = structure._plane
+        bonds = structure._bonds
+        charges = structure._charges
+        radicals = structure._radicals
+        if self.__is_cgr:
+            p_charges = structure._p_charges
+            p_radicals = structure._p_radicals
 
-        bonds = []
-        for n, m, bond in products.bonds():
-            if is_cgr:
-                bonds.append((n, m, {'order': bond.order, 'p_order': bond.p_order,
-                                     'stereo': bond.stereo, 'p_stereo': bond.p_stereo}))
-            else:
-                bonds.append((n, m, {'order': bond.order, 'stereo': bond.stereo}))
-
-        return reactants, dict(absolute_atom), bonds, conditional_element, is_cgr, to_delete
-
-    def patcher(self, structure, mapping):
-        new = type(structure)()
+        new = structure.__class__()
         new.meta.update(self.__meta)
-        to_delete = {mapping[x] for x in self.__to_delete}
-        atoms = {}
-        new_atoms = {}
 
+        to_delete = {mapping[x] for x in self.__to_delete}
         if to_delete:
             # if deleted atoms have another path to remain fragment, the path is preserved
             remain = set(mapping.values()).difference(to_delete)
             delete, global_seen = set(), set()
             for x in to_delete:
-                for n in structure.adj[x]:
+                for n in bonds[x]:
                     if n in global_seen or n in remain:
                         continue
-                    seen = set()
-                    seen.add(n)
+                    seen = {n}
                     global_seen.add(n)
-                    stack = [x for x in structure.adj[n] if x not in global_seen]
+                    stack = [x for x in bonds[n] if x not in global_seen]
                     while stack:
                         current = stack.pop()
                         if current in remain:
@@ -184,40 +134,45 @@ class CGRreactor:
                             continue
                         seen.add(current)
                         global_seen.add(current)
-                        stack.extend([x for x in structure.adj[current] if x not in global_seen])
+                        stack.extend([x for x in bonds[current] if x not in global_seen])
                     else:
                         delete.update(seen)
 
             to_delete.update(delete)
 
+        new_atoms = {}
         for n, atom in self.__atom_attrs.items():
-            if n in mapping:
-                n = mapping[n]
-                atoms[n] = {**structure.atom(n), **atom}
-            else:
-                new_atoms[n] = atom
-        for n, element in self.__conditional_element.items():
-            n = mapping[n]
-            atoms[n]['element'] = element[structure.atom(n).element]
-        for n, atom in atoms.items():
-            new.add_atom(atom, n)
-        for n, atom in structure.atoms():  # add unmatched atoms
-            if n not in atoms and n not in to_delete:
-                new.add_atom(atom, n)
+            if n in mapping:  # add matched atoms
+                m = mapping[n]
+                new.add_atom(elements[n].copy(), m, xy=plane[m], **atom)
+            else:  # new atoms
+                new_atoms[n] = {'atom': elements[n].copy(), **atom}
+
+        old_atoms = set(new._atoms)
+        if self.__is_cgr:
+            for n, atom in structure.atoms():  # add unmatched atoms
+                if n not in old_atoms and n not in to_delete:
+                    new.add_atom(atom.copy(), n, charge=charges[n], is_radical=radicals[n], xy=plane[n],
+                                 p_is_radical=p_radicals[n], p_charge=p_charges[n])
+        else:
+            for n, atom in structure.atoms():  # add unmatched atoms
+                if n not in old_atoms and n not in to_delete:
+                    new.add_atom(atom.copy(), n, charge=charges[n], is_radical=radicals[n],  xy=plane[n])
+
         for n, atom in new_atoms.items():
-            mapping[n] = new.add_atom(atom)
+            mapping[n] = new.add_atom(**atom)
 
         for n, m, bond in self.__bond_attrs:  # add patch bonds
             n = mapping[n]
             m = mapping[m]
             new.add_bond(n, m, bond)
 
-        for n, m_bond in structure._adj.items():
+        for n, m_bond in bonds.items():
             if n in to_delete:  # atoms for removing
                 continue
             to_delete.add(n)
             for m, bond in m_bond.items():
-                if m in to_delete or n in atoms and m in atoms:
+                if m in to_delete or n in old_atoms and m in old_atoms:
                     continue
                 new.add_bond(n, m, bond)
 
@@ -375,4 +330,4 @@ def skip(mapping):
         yield m
 
 
-__all__ = ['CGRreactor', 'Reactor']
+__all__ = ['CGRReactor', 'Reactor']
