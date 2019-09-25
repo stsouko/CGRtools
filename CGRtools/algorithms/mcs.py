409ade6f2b33e3495eb98e67b05b38a359ec5246
stsouko <stsouko@live.ru>
2019-09-25 17:55:59 +0300

    saved

--- CGRtools/algorithms/mcs.py
+++ CGRtools/algorithms/mcs.py
@@ -18,7 +18,7 @@
 #
 from abc import abstractmethod
 from collections import defaultdict
-from itertools import product, combinations, islice
+from itertools import product, combinations, islice, permutations
 from typing import Dict, Set, Optional, Iterator, Tuple, Type
 
 
@@ -26,7 +26,7 @@ class MCS:
     __slots__ = ()
 
     @abstractmethod
-    def get_mcs_mapping(self, other) -> Dict[int, int]:
+    def get_mcs_mapping(self, other, *, automorphism_filter: bool = True) -> Dict[int, int]:
         product_graph = self.__get_product(other)
 
         def calc_bonds(c):
@@ -40,20 +40,31 @@ class MCS:
             return bond_sum
 
         cliques = self.__clique(product_graph)
-        mapping = [(c, calc_bonds(c)) for c in islice(cliques, 10)]
-        max_sum = max(x for _, x in mapping)
-        yield from (x for x, y in mapping if y == max_sum)
+        yield from (dict(c) for c in cliques)
+        #mapping = [(c, calc_bonds(c)) for c in islice(cliques, 42)]
+        #max_bonds = max(x for _, x in mapping)
+        #max_atoms = max(len(x) for x, y in mapping if y == max_bonds)
+        #yield from (dict(x) for x, y in mapping if y == max_bonds and len(x) == max_atoms)
 
-        for clique in self.__clique(product_graph):
-            if bond_sum > max_sum:
-                max_sum = bond_sum
-                if i > 9:
-                    yield dict(clique)
-
+        #br = 0
+        #for clique in cliques:
+        #    if len(clique) < max_atoms:
+        #        br += 1
+        #        if br > 10:
+        #            break
+        #        continue
+        #    elif br:
+        #        br = 0
+        #    bond_sum = calc_bonds(clique)
+        #    if bond_sum > max_bonds:
+        #        max_bonds = bond_sum
+        #        max_atoms = len(clique)
+        #        yield dict(clique)
+        #    elif bond_sum == max_bonds:
+        #        yield dict(clique)
 
     @staticmethod
-    def __clique(graph: Dict[Tuple[int, int], Dict[Tuple[int, int], Tuple[Optional[Type], Optional[Type]]]]) -> \
-            Iterator[Set[Tuple[int, int]]]:
+    def __clique(graph: Dict[Tuple[int, int], Set[Tuple[int, int]]]) -> Iterator[Set[Tuple[int, int]]]:
         """
         clique search
 
@@ -69,14 +80,14 @@ class MCS:
         stack = []
         clique_atoms = [None]
         candidates = subgraph.copy()
-        roots = candidates - graph[max(subgraph, key=lambda x: len(graph[x]))].keys()
+        roots = candidates - graph[max(subgraph, key=lambda x: len(graph[x]))]
 
         while True:
             if roots:
                 root = roots.pop()
                 candidates.remove(root)
                 clique_atoms[-1] = root
-                neighbors = graph[root].keys()
+                neighbors = graph[root]
                 neighbors_subgraph = subgraph & neighbors
                 if not neighbors_subgraph:
                     yield set(clique_atoms)
@@ -87,46 +98,51 @@ class MCS:
                         clique_atoms.append(None)
                         subgraph = neighbors_subgraph
                         candidates = neighbors_candidates
-                        roots = candidates - graph[max(subgraph,
-                                                       key=lambda x: len(candidates & graph[x].keys()))].keys()
+                        roots = candidates - graph[max(subgraph, key=lambda x: len(candidates & graph[x]))]
             elif not stack:
                 return
             else:
                 clique_atoms.pop()
                 subgraph, candidates, roots = stack.pop()
 
-    def __get_product(self, other) -> Dict[Tuple[int, int],
-                                           Dict[Tuple[int, int], Tuple[Optional[Type], Optional[Type]]]]:
+    def __get_product(self, other) -> Dict[Tuple[int, int], Set[Tuple[int, int]]]:
         bonds = self._bonds
         o_bonds = other._bonds
 
-        s_equal = defaultdict(set)  # equal self atoms. from n - 1 to (n^2 - n)/2 complexity
+        s_equal = defaultdict(set)  # equal self atoms
         for n, atom in self._atoms.items():
             s_equal[atom].add(n)
+        p_equal = defaultdict(set)  # equal other atoms
+        for n, atom in other._atoms.items():
+            p_equal[atom].add(n)
 
         product_graph = {}
-        equal_atoms = defaultdict(set)
-        for n, atom in other._atoms.items():
-            for m in s_equal.get(atom, ()):
-                product_graph[(m, n)] = {}
-                equal_atoms[m].add(n)
+        equal_atoms = {}
+        for atom, ns in s_equal.items():
+            ms = p_equal[atom]
+            if ms:
+                for nm in product(ns, ms):
+                    product_graph[nm] = set()
+                for n in ns:
+                    equal_atoms[n] = ms  # memory save
 
         for (n, ns_other), (m, ms_other) in combinations(equal_atoms.items(), 2):
             bond = bonds[n].get(m)
-            if not bond:
-                for o_n, o_m in product(ns_other, ms_other):
-                    if o_n != o_m:
-                        node1 = (n, o_n)
-                        node2 = (m, o_m)
-                        product_graph[node1][node2] = product_graph[node2][node1] = (bond, o_bonds[o_n].get(o_m))
-            else:
+            if bond:
                 for o_n, o_m in product(ns_other, ms_other):
                     if o_n != o_m:
                         o_bond = o_bonds[o_n].get(o_m)
-                        if not o_bond or bond == o_bond:
+                        if bond == o_bond:
                             node1 = (n, o_n)
                             node2 = (m, o_m)
-                            product_graph[node1][node2] = product_graph[node2][node1] = (bond, o_bond)
+                            product_graph[node1].add(node2)
+                            product_graph[node2].add(node1)
+        for ms in product_graph.values():
+            if len(ms) > 1:
+                for n, m in combinations(ms, 2):
+                    if m not in product_graph[n]:
+                        product_graph[n].add(m)
+                        product_graph[m].add(n)
         return product_graph
 
 
