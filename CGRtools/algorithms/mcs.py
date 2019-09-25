b2bb6c4eca3716ea6a5802194ef7fae1a64c2dbf
stsouko <stsouko@live.ru>
2019-09-25 12:12:45 +0300

    save

--- CGRtools/algorithms/mcs.py
+++ CGRtools/algorithms/mcs.py
@@ -18,8 +18,8 @@
 #
 from abc import abstractmethod
 from collections import defaultdict
-from itertools import product, combinations
-from typing import Dict, List, Set, Hashable, Iterator, Tuple
+from itertools import product, combinations, islice
+from typing import Dict, Set, Optional, Iterator, Tuple, Type
 
 
 class MCS:
@@ -27,16 +27,33 @@ class MCS:
 
     @abstractmethod
     def get_mcs_mapping(self, other) -> Dict[int, int]:
-        bonds = self._bonds
-        o_bonds = self._bonds
         product_graph = self.__get_product(other)
-        mapping = {}
+
+        def calc_bonds(c):
+            seen = set()
+            bond_sum = 0
+            for n in c:
+                seen.add(n)
+                for m, (b, o_b) in product_graph[n].items():
+                    if m not in seen and m in c and b == o_b is not None:
+                        bond_sum += 1
+            return bond_sum
+
+        cliques = self.__clique(product_graph)
+        mapping = [(c, calc_bonds(c)) for c in islice(cliques, 10)]
+        max_sum = max(x for _, x in mapping)
+        yield from (x for x, y in mapping if y == max_sum)
+
         for clique in self.__clique(product_graph):
-            pass  # todo: bond filter
-        return mapping
+            if bond_sum > max_sum:
+                max_sum = bond_sum
+                if i > 9:
+                    yield dict(clique)
+
 
     @staticmethod
-    def __clique(graph: Dict[Tuple[int, int], Set[Tuple[int, int]]]) -> Iterator[Dict[int, int]]:
+    def __clique(graph: Dict[Tuple[int, int], Dict[Tuple[int, int], Tuple[Optional[Type], Optional[Type]]]]) -> \
+            Iterator[Set[Tuple[int, int]]]:
         """
         clique search
 
@@ -46,23 +63,23 @@ class MCS:
         if not subgraph:
             return  # empty or fully disconnected
         elif len(subgraph) == 2:  # dimer
-            yield dict(subgraph)
+            yield set(subgraph)
             return
 
         stack = []
         clique_atoms = [None]
         candidates = subgraph.copy()
-        roots = candidates - graph[max(subgraph, key=lambda x: len(graph[x]))]
+        roots = candidates - graph[max(subgraph, key=lambda x: len(graph[x]))].keys()
 
         while True:
             if roots:
                 root = roots.pop()
                 candidates.remove(root)
                 clique_atoms[-1] = root
-                neighbors = graph[root]
+                neighbors = graph[root].keys()
                 neighbors_subgraph = subgraph & neighbors
                 if not neighbors_subgraph:
-                    yield dict(clique_atoms)
+                    yield set(clique_atoms)
                 else:
                     neighbors_candidates = candidates & neighbors
                     if neighbors_candidates:
@@ -70,14 +87,16 @@ class MCS:
                         clique_atoms.append(None)
                         subgraph = neighbors_subgraph
                         candidates = neighbors_candidates
-                        roots = candidates - graph[max(subgraph, key=lambda x: len(candidates & graph[x]))]
+                        roots = candidates - graph[max(subgraph,
+                                                       key=lambda x: len(candidates & graph[x].keys()))].keys()
             elif not stack:
                 return
             else:
                 clique_atoms.pop()
                 subgraph, candidates, roots = stack.pop()
 
-    def __get_product(self, other) -> Dict[Tuple[int, int], Set[Tuple[int, int]]]:
+    def __get_product(self, other) -> Dict[Tuple[int, int],
+                                           Dict[Tuple[int, int], Tuple[Optional[Type], Optional[Type]]]]:
         bonds = self._bonds
         o_bonds = other._bonds
 
@@ -89,7 +108,7 @@ class MCS:
         equal_atoms = defaultdict(set)
         for n, atom in other._atoms.items():
             for m in s_equal.get(atom, ()):
-                product_graph[(m, n)] = set()
+                product_graph[(m, n)] = {}
                 equal_atoms[m].add(n)
 
         for (n, ns_other), (m, ms_other) in combinations(equal_atoms.items(), 2):
@@ -99,8 +118,7 @@ class MCS:
                     if o_n != o_m:
                         node1 = (n, o_n)
                         node2 = (m, o_m)
-                        product_graph[node1].add(node2)
-                        product_graph[node2].add(node1)
+                        product_graph[node1][node2] = product_graph[node2][node1] = (bond, o_bonds[o_n].get(o_m))
             else:
                 for o_n, o_m in product(ns_other, ms_other):
                     if o_n != o_m:
@@ -108,8 +126,7 @@ class MCS:
                         if not o_bond or bond == o_bond:
                             node1 = (n, o_n)
                             node2 = (m, o_m)
-                            product_graph[node1].add(node2)
-                            product_graph[node2].add(node1)
+                            product_graph[node1][node2] = product_graph[node2][node1] = (bond, o_bond)
         return product_graph
 
 
