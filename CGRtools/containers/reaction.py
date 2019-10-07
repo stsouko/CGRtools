18c6b23e1d8ded04f4cf7d5da49a9edee6632826
ravil <sonic-mc@mail.ru>
2019-10-07 12:38:07 +0300

    перезалив

--- CGRtools/containers/reaction.py
+++ CGRtools/containers/reaction.py
@@ -122,6 +122,47 @@ class ReactionContainer(DepictReaction):
                               products=(x.copy() for x in self.__products),
                               reactants=(x.copy() for x in self.__reactants))
 
+    def centers_list(self) -> Tuple[Tuple[int, ...], ...]:
+        """
+        union reaction centers by leaving or substitute group
+
+        :return: list of reaction centers
+        """
+        reactants = reduce(or_, self.reactants)
+        products = reduce(or_, self.products)
+        cgr = reactants ^ products
+        protective_groups = set(reactants).difference(products)
+        comming_groups = set(products).difference(reactants)
+
+        if protective_groups:
+            protective_group = cgr.substructure(protective_groups).connected_components
+            protective_groups_list = [set(x) for x in protective_group]
+        else:
+            protective_groups_list = []
+        if comming_groups:
+            comming_group = cgr.substructure(comming_groups).connected_components
+            comming_groups_list = [set(x) for x in comming_group]
+        else:
+            comming_groups_list = []
+
+        all_groups = protective_groups_list + comming_groups_list
+        out_list = list(cgr.centers_list)
+
+        for x in all_groups:
+            intersection = []
+            for i, y in enumerate(out_list):
+                if y.intersection(x):
+                    intersection.append(i)
+
+            if len(intersection) > 1:
+                union = set()
+                for i in reversed(intersection):
+                    union.update(out_list[i])
+                    out_list.pop(i)
+                out_list.append(list(union))
+
+        return out_list
+
     def implicify_hydrogens(self) -> int:
         """
         remove explicit hydrogens if possible
