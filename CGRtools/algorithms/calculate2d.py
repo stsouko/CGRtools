3cc5adeedc2d58f2e6a1c57aaf138603b9a99446
dinar <batyrshin-dinar@mail.ru>
2019-10-15 15:54:06 +0300

    for A*

--- CGRtools/algorithms/calculate2d.py
+++ CGRtools/algorithms/calculate2d.py
@@ -17,6 +17,7 @@
 #  along with this program; if not, see <https://www.gnu.org/licenses/>.
 #
 from CachedMethods import cached_property
+from collections import defaultdict
 from itertools import product
 from math import hypot, pi, acos, cos, sin, ceil, atan2
 from ..algorithms.depict import rotate_vector
@@ -41,206 +42,9 @@ def dfs_paths(graph, start, goal):
             else:
                 stack.append((next, path + [next]))
 
-# def optimal(temps, a, b, c):
-#     ax, ay = a
-#     bx, by = b
-#     cx, cy = c
-#     min_distance = 10000
-#     fa = fc = 0
-#     for temp in temps:
-#         t1, t2 = temp
-#         tx1, ty1 = t1
-#         tx2, ty2 = t2
-#
-#
-#         tx1, ty1, tx2, ty2 = tx1 + bx, ty1 + by, tx2 + bx, ty2 + by
-#         fa_1, fc_1 = (tx1 - ax, ty1 - ay), (tx2 - cx, ty2 - cy)
-#         fa_2, fc_2 = (tx1 - cx, ty1 - cy), (tx2 - ax, ty2 - ay)
-#         sum_1 = hypot(*fa_1) + hypot(*fc_1)
-#         sum_2 = hypot(*fa_2) + hypot(*fc_2)
-#         summ = min(sum_1, sum_2)
-#         if summ < min_distance:
-#             min_distance = summ
-#             if summ == sum_1:
-#                 fa, fc = fa_1, fc_1
-#             else:
-#                 fa, fc = fa_2, fc_2
-#     return fa, fc
-
 
 class Calculate2D:
     __slots__ = ()
-
-    @cached_property
-    def __angles(self):
-        bonds = self._bonds
-        sssr = self.sssr
-        angles = []
-        for n, m_bond in bonds.items():
-            neighbors = tuple(m_bond)
-            ln = len(m_bond)
-            cycle_angle = None
-            for cycle in sssr:
-                lc = len(cycle)
-                if n in cycle:
-                    if lc == 3:
-                        cycle_angle = pi / 3
-                    else:
-                        cycle_angle = pi - 2 * pi / lc
-            if ln == 2:
-                a, c = neighbors
-                ange = normal_angles[bonds[a][n].order][bonds[n][c].order]
-                if cycle_angle:
-                    ange = cycle_angle
-                angles.append((a, n, c, ange))
-            # elif ln == 4:
-            #     print(m_bond)
-            #     print(neighbors)
-            #     angles.append((neighbors[0], n, neighbors[3], pi))
-            #     angles.append((neighbors[1], n, neighbors[2], pi))
-            #     angles.append((neighbors[0], n, neighbors[2], pi / 2))
-            #     angles.append((neighbors[2], n, neighbors[3], pi / 2))
-            #     angles.append((neighbors[3], n, neighbors[1], pi / 2))
-            #     angles.append((neighbors[1], n, neighbors[0], pi / 2))
-            #     print(angles)
-            #     # for v, w in zip(neighbors, neighbors[1:]):
-            #     #     angles.append((v, n, w, pi / 2))
-            # elif ln != 1:
-            #     ange = 2 * pi / ln
-            #     if cycle_angle:
-            #         ange = cycle_angle
-            #     angles.append((neighbors[-1], n, neighbors[0], ange))
-            #     for v, w in zip(neighbors, neighbors[1:]):
-            #         angles.append((v, n, w, ange))
-        return angles
-
-    def __get_dist_forces(self):
-        plane = self._plane
-
-        # for hypot(x, y)
-        for n, (x, y) in plane.items():
-            if not x and not y:
-                plane[n] = (.0001, .0001)
-        force_fields = {k: (.00001, .00001) for k in self._atoms}
-
-        # distance forces
-        max_diff = 0.1
-        for n, m, bond in self.bonds():
-            nx, ny = plane[n]
-            mx, my = plane[m]
-            x, y = nx - mx, ny - my
-            c_dist = hypot(x, y)
-
-            diff = abs(n_dist - c_dist)
-            if diff > max_diff:
-                max_diff = diff
-
-            elong = n_dist / c_dist / 2 - .5
-            dx, dy = x * elong, y * elong
-            x, y = force_fields[n]
-            force_fields[n] = (x + dx, y + dy)
-            x, y = force_fields[m]
-            force_fields[m] = (x - dx, y - dy)
-
-        return force_fields, max_diff
-
-    def __get_angle_forces(self):
-        # angle forces
-        #
-        # a
-        # |
-        # b---c
-        #
-        plane = self._plane
-        force_fields = {k: (.00001, .00001) for k in self._atoms}
-
-        max_diff = 0.1
-        print(self.__angles)
-        for a, b, c, opt in self.__angles:
-            ax, ay = plane[a]
-            bx, by = plane[b]
-            cx, cy = plane[c]
-            bax, bay = ax - bx, ay - by
-            bcx, bcy = cx - bx, cy - by
-
-            l_ba, l_bc = hypot(bay, bax), hypot(bcy, bcx)
-            bax /= l_ba
-            bay /= l_ba
-            bcx /= l_bc
-            bcy /= l_bc
-
-            # bis_x = bax + bcx
-            # bis_y = bay + bcy
-            # bis_l = hypot(bis_y, bis_x)
-            angle = acos(bax * bcx + bay * bcy)
-            if angle < .01:
-                dx, dy = rotate_vector(0, .2, bcx, bcy)
-                fax, fay = force_fields[a]
-                force_fields[a] = fax + dx, fay + dy
-                fcx, fcy = force_fields[c]
-                force_fields[c] = fcx - dx, fcy - dy
-
-            diff = angle - opt
-            adiff = abs(diff)
-            if adiff > max_diff:
-                max_diff = adiff
-
-            rx, ry = rotate_vector2(bcx, bcy, diff)
-            rx, ry = rx + bx, ry + by
-            fx, fy = rx - cx, ry - cy
-            # force = 2 * (opt - angle) * adiff
-            # ratio = force / bis_l
-            # bis_x *= ratio
-            # bis_y *= ratio
-
-            kx, ky = force_fields[b]
-            force_fields[b] = (kx + fx, ky + fy)
-            # for n, m_bond in bonds.items():
-            #     ln = len(m_bond)
-            #     nx, ny = plane[n]
-            #     if ln == 1:
-            #         m, = m_bond
-            #         mx, my = plane[m]
-            #         min_distance = 10000
-            #         fx = fy = 0
-            #         for temp in templates_1:
-            #             tx, ty = temp
-            #             tx, ty = tx + nx, ty + ny
-            #             fm = (tx - mx, ty - my)
-            #             summ = hypot(*fm)
-            #             if summ < min_distance:
-            #                 min_distance = summ
-            #                 fx, fy = fm
-            #         fmx, fmy = force_fields[m]
-            #         force_fields[m] = fmx + fx, fmy + fy
-        return force_fields, max_diff
-
-    def _changes(self, forces):
-        """
-        changes in coordinates
-        :param forces: dict of atoms forces
-        :return:
-        """
-        plane = self._plane
-
-        stack = 1
-        steps = 1
-        while stack:
-            # for x in range(1):
-            stack = 0
-            force = max(hypot(x, y) for x, y in forces.values())
-            ratio = .2 / force
-            if ratio > 1:
-                ratio = 1
-            for atom, (x2, y2) in forces.items():
-                x1, y1 = plane[atom]
-                x2, y2 = x2 * ratio, y2 * ratio
-                plane[atom] = (x1 + x2, y1 + y2)
-            if force > .05:
-                stack = 1
-            if steps >= 200:
-                break
-            steps += 1
 
     def calculate_cycles(self):
         cycles = self.sssr
@@ -382,116 +186,66 @@ class Calculate2D:
                 if count:
                     stack.append((index + 1, (x2, y2), count))
 
+    @cached_property
+    def __compiled(self):
+        atoms = self._atoms
+        bonds = self._bonds
+        atoms_order = self.atoms_order
+
+        closures = defaultdict(list)
+        components = []
+        seen = set()
+        while len(seen) < len(atoms):
+            start = max(atoms.keys() - seen, key=lambda x: atoms_order[x])
+            seen.add(start)
+            stack = [(n, start, bond) for n, bond in sorted(bonds[start].items(),
+                                                            key=lambda x: atoms_order[x[0]])]
+            order = [start]
+            components.append(order)
+
+            while stack:
+                front, back, *_ = atom = stack.pop()
+                if front not in seen:
+                    order.append(atom)
+                    for n, bond in sorted(bonds[front].items(), key=lambda x: atoms_order[x[0]]):
+                        if n != back:
+                            if n not in seen:
+                                stack.append((n, front, bond))
+                            else:
+                                closures[front].append((n, bond))
+                    seen.add(front)
+        return components, closures
+
     def calculate_chains(self):
         plane = self._plane
-        atoms = set(self._atoms)
+        atoms = self._atoms
         bonds = self._bonds
 
-        if bonds:
-            singles = [k for k, v in bonds.items() if len(v) == 1]
-        else:
-            raise NotImplemented('can`t clean for one atom')
-        pairs = list(product(singles, singles))
-        max_dist = 0
-        max_path = None
-        for s, e in pairs:
-            if not s == e:
-                for path in dfs_paths(bonds, s, e):
-                    num = len(path)
-                    if num > max_dist:
-                        max_dist = num
-                        max_path = path
+        components, closures = self.__compiled
+        for chain in components:
+            dx, dy = n_dist * sin(pi / 3), n_dist / 2
 
-        if max_dist == 2:
-            n, m = max_path
-            plane[n] = (0, 0)
-            plane[m] = (n_dist, 0)
-        else:
-            dict_for_single = {}
-            atom1 = max_path[0]
-            atom2 = max_path[1]
-            atom3 = max_path[2]
-            dy, dx = n_dist / 2, sin(pi / 3) * n_dist
-            n, m, o = (-dx, dy), (0, 0), (dx, -dy)
-            bond2 = bonds[atom2][atom3].order
-            if normal_angles[bonds[atom1][atom2].order][bond2] != pi:
-                n, m, o = (-dx, -dy / 2), (0, dy / 2), (dx, - dy / 2)
-            plane[atom1], plane[atom2], plane[atom3] = n, m, o
-            nx, ny = n
-            mx, my = m
-            ox, oy = o
-            vx, vy = ox - mx, oy - my
-            dict_for_single[atom2] = (mx - nx, my - ny)
-            dict_for_single[atom3] = (vx, vy)
-            seen = {atom1, atom2}
-            stack = [(2, o, bond2, (vx, vy), max_dist - 2)]
-            print(max_path)
+            start = chain[0]
+            stack = [[(next_atom, start, bond.order, 0)] for next_atom, prev_atom, bond in chain[1:]
+                     if prev_atom == start]
+            path = []
+            hashed_path = set()
             while stack:
-                index, coords, bnd1, v, count = stack.pop(0)
-                atm1 = max_path[index]
-                count -= 1
-                x1, y1 = coords
-                v_x, v_y = v
-                if atm1 not in seen:
-                    seen.add(atm1)
-                    nxt = index + 1
-                    if nxt < max_dist:
-                        atm2 = max_path[nxt]
-                        bnd2 = bonds[atm1][atm2].order
-                        ang = atan2(v_y, v_x)
-                        if normal_angles[bnd1][bnd2] == pi:
-                            if ang:
-                                x2 = x1 + dx
-                                if ang > 0:
-                                    y2 = y1 + dy
-                                else:
-                                    y2 = y1 - dy
-                            else:
-                                x2 = x1 + n_dist
-                                y2 = y1
-                        else:
-                            if ang:
-                                x2 = x1 + dx
-                                if ang > 0:
-                                    y2 = y1 - dy
-                                else:
-                                    y2 = y1 + dy
-                            else:
-                                x2 = x1 + dx
-                                y2 = y1 + dy
-                        plane[atm2] = (x2, y2)
-                        if count:
-                            v = (x2 - x1, y2 - y1)
-                            dict_for_single[atm2] = v
-                            stack.append((nxt, (x2, y2), bnd2, v, count))
+                atom, prev_atom, bond, _ = stack[-1].pop()
+                if prev_atom == start:
+                    plane[atom], plane[prev_atom] = (dx, dy), (0, 0)
+                path.append((atom, prev_atom, bond))
+                hashed_path.add(atom)
 
-            lasts = atoms - seen
-            kv = {}
-            was = None
-            for atm in lasts:
-                atm3 = list(bonds[atm])[0]
-                if atm3 not in dict_for_single:
-                    continue
-                if atm3 not in kv:
-                    kv[atm3] = atm
-                else:
-                    was = kv[atm3]
-                vx, vy = dict_for_single[atm3]
-                x1, y1 = plane[atm3]
-                ang = atan2(vy, vx)
-                if ang > 0:
-                    y2 = y1 + n_dist
-                else:
-                    y2 = y1 - n_dist
-                if was:
-                    x2 = x1 - 2/3 * dx
-                    x3 = x1 + 2/3 * dx
-                    plane[was] = (x3, y2)
-                    was = None
-                else:
-                    x2 = x1
+                if len(path) == 1:
+                    del stack[-1]
+                    if stack:
+                        path = path[:stack[-1][-1][-1]]
+                        hashed_path = {x for x, *_ in path}
+                    break
 
-                plane[atm] = (x2, y2)
+                elif atom != start:
+                    pass
 
     def clean2d(self):
         cycles = self.sssr
@@ -518,9 +272,9 @@ templates_3 = [((0, n_dist), (dist_3_la, -dist_3_sm), (-dist_3_la, -dist_3_sm)),
                ((n_dist, 0), (-dist_3_sm, -dist_3_la), (-dist_3_sm, dist_3_la))]
 templates_4 = [((-n_dist, 0), (0, n_dist), (n_dist, 0), (0, -n_dist)),
                ((-dist_4, dist_4), (dist_4, dist_4), (dist_4, -dist_4), (-dist_4, -dist_4))]
-normal_angles = {1: {1: 2/3*pi, 2: 2/3*pi, 3: pi, 4: pi},
-                 2: {1: 2/3*pi, 2: pi, 3: pi, 4: pi},
-                 3: {1: pi, 2: pi, 3: pi, 4: pi},
-                 4: {1: pi, 2: pi, 3: pi, 4: pi}}
+normal_angles = {1: {1: False, 2: False, 3: True, 4: True},
+                 2: {1: False, 2: True, 3: True, 4: True},
+                 3: {1: True, 2: True, 3: True, 4: True},
+                 4: {1: True, 2: True, 3: True, 4: True}}
 
 __all__ = ['Calculate2D']
