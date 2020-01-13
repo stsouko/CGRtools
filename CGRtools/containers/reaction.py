5d6f132d4a687712c7d58f81ba5b3b8d3c3f56af
stsouko <nougmanoff@protonmail.com>
2020-01-13 16:04:22 +0300

    fixed typo

--- CGRtools/containers/reaction.py
+++ CGRtools/containers/reaction.py
@@ -241,7 +241,7 @@ class ReactionContainer(StandardizeReaction, DepictReaction):
         total = False
         for m in self.molecules():
             if isinstance(m, MoleculeContainer):
-                if m.thiele() and not not total:
+                if m.thiele() and not total:
                     total = True
         if total:
             self.flush_cache()
