5e5b9ea532c995f9a15afa9874a91dcc8e0606bf
dinar <batyrshin-dinar@mail.ru>
2020-01-23 16:58:02 +0300

    step 1

--- CGRtools/algorithms/calculate2d.py
+++ CGRtools/algorithms/calculate2d.py
@@ -16,67 +16,110 @@
 #  You should have received a copy of the GNU Lesser General Public License
 #  along with this program; if not, see <https://www.gnu.org/licenses/>.
 #
-from networkx.drawing.layout import kamada_kawai_layout
+from array import array
+from CachedMethods import cached_property
+from collections import defaultdict
+from math import hypot, pi, acos, cos, sin, sqrt
+from ..algorithms.depict import rotate_vector
 from random import uniform
 
 
+def rotate_vector2(x1, y1, angle):
+    """
+    rotate x,y vector over angle
+    """
+    cos_rad = cos(angle)
+    sin_rad = sin(angle)
+    return cos_rad * x1 + sin_rad * y1, -sin_rad * x1 + cos_rad * y1
+
+
 class Calculate2D:
-    def calculate2d(self, force=False, scale=1):
+    __slots__ = ()
+
+    def _update(self):
+        atoms = self._atoms
+        bonds = list(self.bonds())
+        mapping = {}
+        dd = defaultdict(dict)
+        update_atoms = list(atoms)
+        xyz_matrix = []
+
+        # add virtual atoms
+        for i, (n, m, bond) in enumerate(bonds):
+            order1 = bond.order
+            dd[n][m] = dd[m][n] = 8.0 if order1 == 8 else 5.0
+            for _n, _m, _bond in bonds[i + 1:]:
+                order2 = _bond.order
+                dd[_n][_m] = dd[_m][_n] = 8.0 if order2 == 8 else 5.0
+                common = {n, m}.intersection({_n, _m})
+                if common:
+                    atom_number = list(common)[0]
+                    atom = atoms[atom_number]
+                    if atom.neighbors == 2 and \
+                            atom.atomic_number in (5, 6, 7, 8, 14, 15, 16, 17, 33, 34, 35, 52, 53, 85) and not \
+                            (order1 == 1 and order2 == 3 or order1 == 3 and order2 == 1 or order1 == 2 and order2 == 2):
+                        dd[atom_number][0] = 5.0
+                        update_atoms.append(0)
+
+        # create matrix of coordinates
+        n = len(update_atoms)
+        n = n / 2
+        ind = 0
+        for atm in update_atoms:
+            if atm:
+                mapping[atm] = ind
+            xyz_matrix.append(array('f', [uniform(-n, n), uniform(-n, n), uniform(-n, n)]))
+            ind += 1
+        return update_atoms, mapping, dd, tuple(xyz_matrix)
+
+    @staticmethod
+    def _distances(atoms, matrix):
+        sum_sqr = 0
+        dists = []
+        for i, a in enumerate(atoms):
+            for _a in atoms[i + 1:]:
+                for g, j in zip(matrix[a], matrix[_a]):
+                    sum_sqr += (int(g) - int(j)) ** 2
+                dists.append([a, _a, sqrt(sum_sqr)])
+        return dists
+
+    def _repulsive_force(self):
+        pass
+
+    def clean2d(self):
         """
-        recalculate 2d coordinates. currently rings can be calculated badly.
-
-        :param scale: rescale calculated positions.
-        :param force: ignore existing coordinates of atoms
+        steps for calculate 2d coordinates
+        :return: None
         """
-        dist = {}
-        # length forces
-        for n, m_bond in self._adj.items():
-            dist[n] = {}
-            for m in m_bond:
-                dist[n][m] = .825
-
-        # angle forces
-        for n, m_bond in self._adj.items():
-            if len(m_bond) == 2:  # single-single or single-double bonds has angle = 120, other 180
-                (m1, b1), (m2, b2) = m_bond.items()
-                dist[m1][m2] = dist[m2][m1] = 1.43 if b1.order + b2.order in (2, 3) else 1.7  # +.05
-            elif len(m_bond) == 3:
-                m1, m2, m3 = m_bond
-                dist[m1][m2] = dist[m1][m3] = dist[m2][m3] = dist[m3][m2] = dist[m2][m1] = dist[m3][m1] = 1.43
-            elif len(m_bond) == 4:
-                #    1
-                #
-                # 2  X  4
-                #
-                #    3
-                m1, m2, m3, m4 = m_bond
-                dist[m1][m2] = dist[m1][m4] = dist[m2][m1] = dist[m2][m3] = 1.17
-                dist[m3][m2] = dist[m3][m4] = dist[m4][m1] = dist[m4][m3] = 1.17
-                dist[m1][m3] = dist[m3][m1] = dist[m2][m4] = dist[m4][m2] = 1.7  # +.05
-
-        # cycle forces
-        for r in self.sssr:
-            if len(r) == 6:
-                #    6
-                #
-                # 1     5
-                #
-                # 2     4
-                #
-                #    3
-                m1, m2, m3, m4, m5, m6 = r
-                dist[m1][m4] = dist[m4][m1] = dist[m2][m5] = dist[m5][m2] = dist[m3][m6] = dist[m6][m3] = 1.7  # +.05
-
-        if force:
-            pos = None
-        else:
-            pos = {n: (atom.x or uniform(0, .01), atom.y or uniform(0, .01)) for n, atom in self.atoms()}
-
-        for n, xy in kamada_kawai_layout(self, dist=dict(dist), pos=pos, scale=scale).items():
-            atom = self._node[n]
-            atom.x, atom.y = xy
-
-        self.flush_cache()
+        up_atoms, mapping, adj, xyz = self._update()
+        dist = self._distances(up_atoms, xyz)
+        # stack = 1
+        # steps = 1
+        # primary_forces, dif_dist = self.__get_dist_forces()
+        # secondary_forces, dif_ang = self.__get_angle_forces()
+        # print(dif_dist, dif_ang)
+        # if dif_ang > dif_dist:
+        #     primary_forces, secondary_forces = secondary_forces, primary_forces
+        #
+        # while stack:
+        #     # for x in range(1):
+        #     stack = 0
+        #     self._changes(primary_forces)
+        #     self._changes(secondary_forces)
+        #
+        #     primary_forces, dif_dist = self.__get_dist_forces()
+        #     secondary_forces, dif_ang = self.__get_angle_forces()
+        #     if dif_ang > dif_dist:
+        #         primary_forces, secondary_forces = secondary_forces, primary_forces
+        #     force_p = max(hypot(x, y) for x, y in primary_forces.values())
+        #     force_s = max(hypot(x, y) for x, y in secondary_forces.values())
+        #     print(force_p, force_s)
+        #     if force_p < .05 and force_s < 0.5:
+        #         stack = 0
+        #
+        #     if steps >= 200:
+        #         break
+        #     steps += 1
 
 
 __all__ = ['Calculate2D']
