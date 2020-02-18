65ca8c2ddd3f82a01d6e6292ed9209debbb45ac7
nasam <samikaevn@yandex.ru>
2020-02-18 14:45:30 +0300

    Tautomers

--- CGRtools/algorithms/tautomers.py
+++ CGRtools/algorithms/tautomers.py
@@ -41,14 +41,40 @@ class Tautomers:
             if bonds[i]:
                 if radicals[i]:             # [B, C, N, O, Si, P, S, Cl, As, Se, Br, Te, I, At]
                     if atom.atomic_number in {5, 6, 7, 8, 14, 15, 16, 17, 33, 34, 35, 52, 53, 85}: # ???
-                        ent_atoms.append((atom, 0)) # 0 - radical
+                        ent_atoms.append((atom, i, 0)) # 0 - radical
                 elif charges[i] < 0:        # [B, C, N, O, Si, P, S, Cl, As, Se, Br, Te, I, At]
                     if atom.atomic_number in {5, 6, 7, 8, 14, 15, 16, 17, 33, 34, 35, 52, 53, 85}: # ???
-                        ent_atoms.append((atom, 1)) # 1 - has a charge
+                        ent_atoms.append((atom, i, 1)) # 1 - has a charge
                 elif hydrogens[i]:          # [B, C, N, O, Si, P, S, Cl, As, Se, Br, Te, I, At]
                     if atom.atomic_number in {5, 6, 7, 8, 14, 15, 16, 17, 33, 34, 35, 52, 53, 85}: # ???
-                        ent_atoms.append((atom, 2)) # 2 - has a hydrogens
+                        ent_atoms.append((atom, i, 2)) # 2 - has a hydrogens
         return ent_atoms
+
+    def get_paths(self, ent_atoms):
+        bonds = self._bonds
+        paths = []
+        for z in ent_atoms:
+            a = z[1]
+            path = [a]
+            stack = []
+            for i, n in bonds[a].items():
+                if n.order < 3:
+                    stack.append((i, 1, n.order))
+            while stack:
+                cur, d, b = stack.pop()
+                if len(path) > d:
+                    path = path[:d]
+                ngbb = bonds[cur]
+                ngb = set(ngbb.keys()).difference(path)
+                for c in ngb:
+                    if (ngbb[c].order < 3) and (ngbb[c].order != b):
+                        stack.append((c, d + 1, ngbb[c].order))
+                        path.append(cur)
+                if not ngb:
+                    path.append(cur)
+                    if len(path) > 2:
+                        paths.append(path)
+        return paths
 
 
 __all__ = ['Tautomers']
