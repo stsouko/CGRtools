a078ffd4d49325a70dff171b42e6d54ae8470e76
dinar <batyrshin-dinar@mail.ru>
2020-03-16 17:21:27 +0300

    fix cumulenes

--- CGRtools/algorithms/depict.py
+++ CGRtools/algorithms/depict.py
@@ -243,8 +243,8 @@ class DepictMolecule(Depict):
 
         # for cumulenes
         cumulenes = {}
-        if self.cumulenes:
-            cumulenes = {y for x in self.cumulenes(atoms_numbers=(6, 7, 8)) for y in x[1:-1] if len(x) > 2}
+        if self._cumulenes((6, 7, 8)):
+            cumulenes = {y for x in self._cumulenes(atoms_numbers=(6, 7, 8)) for y in x[1:-1] if len(x) > 2}
 
         svg = []
         maps = []
@@ -756,7 +756,7 @@ class DepictQuery(Depict):
         # for cumulenes
         cumulenes = {}
         if self._cumulenes((6, 7, 8)):
-            cumulenes = {y for x in self.cumulenes((6, 7, 8)) for y in x[1:-1] if len(x) > 2}
+            cumulenes = {y for x in self._cumulenes((6, 7, 8)) for y in x[1:-1] if len(x) > 2}
 
         svg = []
         mask = []
