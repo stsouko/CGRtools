7303b4cfd3b915313f068fb68c9b8402be213e55
stsouko <nougmanoff@protonmail.com>
2020-03-23 13:29:34 +0300

    reviewed

--- CGRtools/algorithms/tautomers.py
+++ CGRtools/algorithms/tautomers.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 #
 #  Copyright 2020 Nail Samikaev <samikaevn@yandex.ru>
+#  Copyright 2020 Ramil Nugmanov <nougmanoff@protonmail.com>
 #  This file is part of CGRtools.
 #
 #  CGRtools is free software; you can redistribute it and/or modify
@@ -16,21 +17,47 @@
 #  You should have received a copy of the GNU Lesser General Public License
 #  along with this program; if not, see <https://www.gnu.org/licenses/>.
 #
+from typing import TYPE_CHECKING, Iterable, Tuple, Iterator
+
+if TYPE_CHECKING:
+    from CGRtools import MoleculeContainer
 
 
 class Tautomers:
     __slots__ = ()
 
-    def enumerate_tautomers(self):
-        atoms = self._atoms
-        bonds = self._bonds
-        charges = self._charges
-        radicals = self._radicals
-        hydrogens = self._hydrogens
+    def tautomerize(self) -> bool:
+        """
+        Convert structure to canonic tautomer form. Return True if structure changed.
+        """
+        # todo: implement
+        return False
+
+    def enumerate_tautomers(self) -> Iterator['MoleculeContainer']:
+        """
+        Enumerate all possible tautomeric forms of molecule. Supported hydrogen, charge and radicals migration through
+        delocalized chain.
+
+        O=C-C[H] <-> [H]O-C=C
+        O=C-C=C-C[H] <-> [H]O-C=C-C=C
+        O=C-C[H]-C=C <-> [H]O-C=C-C=C
+        O=C-C[H]-C=C <X> O=C-C=C-C[H]  not directly possible
+        [C-]-C=C <-> C=C-[C-], [C-]=C=C <-> C#C-[C-]
+        """
+        # todo: implement
         yield self
 
+    def __entries(self) -> Iterable[Tuple[int, bool]]:
+        # possible: not radicals
+        # O S Se -H[enol] (only 1 neighbor)
+        # N -H[enol] (1 or 2 neighbor)
+        # P -H[enol] (1 or 2 non-hydrogen bonds with order sum 1 or 2)
 
-    def input_atoms(self):
+        # reverse (has double bonded neighbor):
+        # O (charge 0)
+        # S Se (only 1 neighbor and charge 0)
+        # N (charge -1 or 0)
+        # P (charge -1 OR charge 0 and neighbors 1 or 2)
         atoms = self._atoms
         bonds = self._bonds
         charges = self._charges
@@ -46,7 +73,6 @@ class Tautomers:
                     if atom.atomic_number in {7, 8, 14, 15, 16, 33, 34, 52}:
                         ent_atoms.append((atom, i, 2))
         return ent_atoms
-
 
     def get_paths(self, ent_atoms):
         bonds = self._bonds
@@ -77,7 +103,6 @@ class Tautomers:
                 d += 1
                 nbg = [(x, y.order, d) for x, y in bonds[cur].items() if (y.order < 3) and (y.order != b)]
                 stack.extend(nbg)
-
 
 
 __all__ = ['Tautomers']
