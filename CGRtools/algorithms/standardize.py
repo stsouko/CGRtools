c1e12cc8e0f0bdc406332e5485b034bacf72bea1
stsouko <nougmanoff@protonmail.com>
2020-06-25 13:37:31 +0300

    azide rules fixed.

--- CGRtools/algorithms/standardize.py
+++ CGRtools/algorithms/standardize.py
@@ -319,7 +319,7 @@ class Standardize:
 
         # Azide
         #
-        #  N- - N+ # N  >>  N = N+ = N-
+        #  A - N- - N+ # N  >> A - N = N+ = N-
         #
         atoms = ({'atom': 'N', 'charge': 1, 'neighbors': 2},
                  {'atom': 'N', 'charge': -1, 'neighbors': 2, 'hybridization': 1}, {'atom': 'N', 'neighbors': 1})
@@ -330,18 +330,18 @@ class Standardize:
 
         # Azide
         #
-        #  N+ # N = N-  >>  N = N+ = N-
+        #  A - N+ # N = N-  >> A - N = N+ = N-
         #
         atoms = ({'atom': 'N', 'neighbors': 2}, {'atom': 'N', 'charge': 1, 'neighbors': 2},
-                 {'atom': 'N', 'charge': -1, 'neighbors': 1})
-        bonds = ((1, 2, 3), (1, 3, 2))
+                 {'atom': 'N', 'charge': -1, 'neighbors': 1}, {'atom': 'A'})
+        bonds = ((1, 2, 3), (1, 3, 2), (2, 4, 1))
         atom_fix = {1: {'charge': 1}, 2: {'charge': 0, 'hybridization': 2}}
         bonds_fix = ((1, 2, 2),)
         rules.append((atoms, bonds, atom_fix, bonds_fix))
 
         # Azide
         #
-        # N = N # N >> N = N+ = N-
+        # A - N = N # N >> A - N = N+ = N-
         #
         atoms = ({'atom': 'N', 'neighbors': 2},
                  {'atom': 'N', 'neighbors': 2, 'hybridization': 2}, {'atom': 'N', 'neighbors': 1})
@@ -352,7 +352,7 @@ class Standardize:
 
         # Azide
         #
-        # - N = N = N >> - N = N+ = N-
+        # A - N = N = N >> A - N = N+ = N-
         #
         atoms = ({'atom': 'N', 'neighbors': 2},
                  {'atom': 'N', 'neighbors': 2, 'hybridization': 2}, {'atom': 'N', 'neighbors': 1})
@@ -363,7 +363,7 @@ class Standardize:
 
         # Azide
         #
-        # - NH - N # N >> - N = N+ = N-
+        # A - NH - N # N >> A - N = N+ = N-
         #
         atoms = ({'atom': 'N', 'neighbors': 2},
                  {'atom': 'N', 'neighbors': 2, 'hybridization': 1}, {'atom': 'N', 'neighbors': 1})
