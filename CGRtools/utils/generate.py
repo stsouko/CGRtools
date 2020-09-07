92b87b68427fb4ec38afa134cd84f05db55b471d
dinar <batyrshin-dianr@mail.ru>
2020-09-07 14:29:20 +0300

    fix3

--- CGRtools/utils/generate.py
+++ CGRtools/utils/generate.py
@@ -1,6 +1,30 @@
+# -*- coding: utf-8 -*-
+#
+#  Copyright 2020 Ramil Nugmanov <nougmanoff@protonmail.com>
+#  Copyright 2020 Dinar Batyrshin <batyrshin-dinar@mail.ru>
+#  This file is part of CGRtools.
+#
+#  CGRtools is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU Lesser General Public License as published by
+#  the Free Software Foundation; either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#  GNU Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public License
+#  along with this program; if not, see <https://www.gnu.org/licenses/>.
 
 
 def augmented(molecule, deep):
+    """
+    :param molecule: MoleculeContainer
+    :param deep: chain length
+
+    return: list of molecule functional groups
+    """
     bonds = molecule._bonds
 
     if deep < 1:
@@ -23,3 +47,6 @@ def augmented(molecule, deep):
                     stack.append((augx, nt))
 
     return response
+
+
+__all__ = ['augmented']
