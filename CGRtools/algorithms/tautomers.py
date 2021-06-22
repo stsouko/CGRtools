ac8ed636a3cc0eb07f6bd4c5eaf0636823b23a45
stsouko <nougmanoff@protonmail.com>
2021-06-22 18:02:57 +0300

    saved

--- CGRtools/algorithms/tautomers.py
+++ CGRtools/algorithms/tautomers.py
@@ -22,6 +22,7 @@ from collections import deque, defaultdict
 from itertools import product, chain, repeat
 from typing import TYPE_CHECKING, Iterator, Union
 from ..containers import query  # cyclic imports resolve
+from ..exceptions import InvalidAromaticRing
 from ..periodictable import ListElement
 
 
@@ -65,7 +66,7 @@ class Tautomers:
         """
         Enumerate all possible tautomeric forms of molecule.
 
-        :param prepare_molecules: Standardize structures before. Aromatization and implicit hydrogens required.
+        :param prepare_molecules: Standardize structures for correct processing.
         :param full: Do full enumeration.
         :param zwitter: Enable acid-base tautomerization
         :param keto_enol: Enable keto-enol tautomerization
@@ -74,7 +75,7 @@ class Tautomers:
         """
         if limit < 2:
             raise ValueError('limit should be greater or equal 2')
-        yield self.copy()
+
         atoms_stereo = self._atoms_stereo
         allenes_stereo = self._allenes_stereo
         cis_trans_stereo = self._cis_trans_stereo
@@ -85,7 +86,17 @@ class Tautomers:
         if prepare_molecules:
             copy.kekule()
             copy.implicify_hydrogens()
-            copy.thiele()  # prevent
+            copy.thiele(fix_metal_organics=False)
+
+        if has_stereo:
+            ster = copy.copy()
+            ster._atoms_stereo.update(atoms_stereo)
+            ster._allenes_stereo.update(allenes_stereo)
+            ster._cis_trans_stereo.update(cis_trans_stereo)
+            ster._fix_stereo()
+            yield ster
+        else:
+            yield copy.copy()
 
         seen = {copy: None}
         queue = deque([copy])
@@ -182,12 +193,10 @@ class Tautomers:
 
         for d, a in product(donors, acceptors):
             mol = self.copy()
-            m_charges = mol._charges
-            m_hydrogens = mol._hydrogens
-            m_hydrogens[d] -= 1
-            m_hydrogens[a] += 1
-            m_charges[d] -= 1
-            m_charges[a] += 1
+            mol._hydrogens[d] -= 1
+            mol._hydrogens[a] += 1
+            mol._charges[d] -= 1
+            mol._charges[a] += 1
 
             # store cached sssr in new molecules for speedup
             mol.__dict__['rings_count'] = rings_count
@@ -225,16 +234,13 @@ class Tautomers:
 
                 mol = self.copy()
                 m_bonds = mol._bonds
-                m_hydrogens = mol._hydrogens
-                m_hybridizations = mol._hybridizations
-
                 for n, m, b in fix:
                     m_bonds[mapping[n]][mapping[m]]._Bond__order = b
 
-                m_hydrogens[a] += 1
-                m_hydrogens[d] -= 1
-                m_hybridizations[a] -= 1
-                m_hybridizations[d] += 1
+                mol._hydrogens[a] += 1
+                mol._hydrogens[d] -= 1
+                mol._hybridizations[a] -= 1
+                mol._hybridizations[d] += 1
 
                 # store cached sssr in new molecules for speedup
                 mol.__dict__['sssr'] = sssr
@@ -242,7 +248,7 @@ class Tautomers:
                 mol.__dict__['__cached_args_method_heteroatoms'] = heteroatoms.copy()
                 mol.__dict__['atoms_rings_sizes'] = atoms_rings_sizes
                 k = mol.kekule()
-                t = mol.thiele()
+                t = mol.thiele(fix_tautomers=False, fix_metal_organics=False)
                 # restore after kekule-thiele
                 if k or t:
                     mol.__dict__['sssr'] = sssr
@@ -255,34 +261,68 @@ class Tautomers:
 
     def _enumerate_hetero_arene_tautomers(self: Union['MoleculeContainer', 'Tautomers']):
         atoms = self._atoms
+        bonds = self._bonds
         hydrogens = self._hydrogens
         charges = self._charges
         radicals = self._radicals
 
-        single_bonded = set()
-        acceptor = []
-        donor = []
-
         rings = defaultdict(list)  # aromatic skeleton
-        for n, m_bond in self._bonds.items():
+        for n, m_bond in bonds.items():
             for m, bond in m_bond.items():
                 if bond.order == 4:
                     rings[n].append(m)
         if not rings:
             return
 
+        acceptors = set()
+        donors = set()
+        single_bonded = set()
         for n, ms in rings.items():
-            if atoms[n].atomic_number in (5, 7, 15) and len(ms) == 2 and not charges[n] and not radicals[n]:
-                # only neutral B, N, P
-                if hydrogens[n]:
-                    donor.append(n)
-                else:
-                    acceptor.append(n)
-
-        components = [x for x in self._connected_components(rings) if x.issuperset(donor) and x.issuperset(acceptor)]
-        if not components:
+            if len(ms) == 2:
+                if atoms[n].atomic_number in (5, 7, 15) and not charges[n] and not radicals[n]:
+                    # only neutral B, N, P
+                    if hydrogens[n]:  # pyrrole
+                        donors.add(n)
+                    elif len(bonds[n]) == 2:  # pyridine
+                        acceptors.add(n)
+                    else:
+                        single_bonded.add(n)
+                elif charges[n] == -1 and atoms[n].atomic_number == 6:  # ferrocene
+                    single_bonded.add(n)
+        if not donors or not acceptors:
             return
 
+        atoms = set(rings)
+        components = []
+        while atoms:
+            start = atoms.pop()
+            component = {start: rings[start]}
+            queue = deque([start])
+            while queue:
+                current = queue.popleft()
+                for n in rings[current]:
+                    if n not in component:
+                        queue.append(n)
+                        component[n] = rings[n]
+
+            atoms.difference_update(component)
+            if donors.isdisjoint(component) or acceptors.isdisjoint(component):
+                continue
+            components.append(component)
+
+        if not components:
+            return
+        for component in components:
+            for d, a in product(component.keys() & donors, component.keys() & acceptors):
+                sb = component.keys() & single_bonded
+                sb.add(a)  # now pyrrole
+                try:
+                    next(self._kekule_component(component, sb, ()))
+                except InvalidAromaticRing:
+                    continue
+                mol = self.copy()
+                mol._hydrogens[d] = 0
+                mol._hydrogens[a] = 1
         yield
 
     @cached_property
