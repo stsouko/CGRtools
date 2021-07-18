c8403d44e23bf719a8c929b72f54949589a805de
stsouko <nougmanoff@protonmail.com>
2021-07-18 18:47:38 +0300

    lets start

--- CGRtools/containers/_unpack.pyx
+++ CGRtools/containers/_unpack.pyx
@@ -1,4 +1,11 @@
 
 
-def unpack(data):
-    return 'hello'
+def unpack(const unsigned char[:] data):
+    cdef unsigned char a, b, c, d
+    cdef unsigned short na, nct
+
+    a, b, c = data[:3]
+    na = a << 4| (b & 0xf0) >> 4
+    nct = (b & 0x0f) << 4 | c
+
+    return na, nct
